38ef05d4625da55200d7781e5fbe5147b74e5e09
SaLaaR HuSyN <shsallu35@gmail.com>
2019-02-21 22:34:26 +0500

    Initial commit

---
+++ LollipopUiDemo/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace LollipopUiDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lollipopButton1 = new LollipopButton();
+            this.lollipopCard1 = new LollipopCard();
+            this.lollipopCheckBox1 = new LollipopCheckBox();
+            this.lollipopFileInput1 = new LollipopFileInput();
+            this.lollipopFlatButton1 = new LollipopFlatButton();
+            this.lollipopFolderInPut1 = new LollipopFolderInPut();
+            this.lollipopLabel1 = new LollipopLabel();
+            this.lollipopProgressBar1 = new LollipopProgressBar();
+            this.lollipopRadioButton1 = new LollipopRadioButton();
+            this.lollipopSmallCard1 = new LollipopSmallCard();
+            this.lollipopToggle1 = new LollipopToggle();
+            this.lollipopTextBox1 = new LollipopTextBox();
+            this.SuspendLayout();
+            // 
+            // lollipopButton1
+            // 
+            this.lollipopButton1.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopButton1.BGColor = "#314150";
+            this.lollipopButton1.FontColor = "#FFF";
+            this.lollipopButton1.Location = new System.Drawing.Point(562, 334);
+            this.lollipopButton1.Name = "lollipopButton1";
+            this.lollipopButton1.Size = new System.Drawing.Size(143, 41);
+            this.lollipopButton1.TabIndex = 0;
+            this.lollipopButton1.Text = "Demo Button";
+            // 
+            // lollipopCard1
+            // 
+            this.lollipopCard1.ButtonColor = "#314150";
+            this.lollipopCard1.ContentColor = "#007ACC";
+            this.lollipopCard1.ContentText = "We Create,We Design, We Develop";
+            this.lollipopCard1.Image = ((System.Drawing.Image)(resources.GetObject("lollipopCard1.Image")));
+            this.lollipopCard1.Location = new System.Drawing.Point(411, 29);
+            this.lollipopCard1.Name = "lollipopCard1";
+            this.lollipopCard1.Size = new System.Drawing.Size(294, 299);
+            this.lollipopCard1.TabIndex = 1;
+            this.lollipopCard1.Text = "C# Ui Academy";
+            this.lollipopCard1.TitleColor = "#314150";
+            // 
+            // lollipopCheckBox1
+            // 
+            this.lollipopCheckBox1.AutoSize = true;
+            this.lollipopCheckBox1.CheckColor = "#314150";
+            this.lollipopCheckBox1.Location = new System.Drawing.Point(562, 393);
+            this.lollipopCheckBox1.Name = "lollipopCheckBox1";
+            this.lollipopCheckBox1.Size = new System.Drawing.Size(206, 20);
+            this.lollipopCheckBox1.TabIndex = 2;
+            this.lollipopCheckBox1.Text = "Accept Terms and Conditions";
+            this.lollipopCheckBox1.UseVisualStyleBackColor = true;
+            // 
+            // lollipopFileInput1
+            // 
+            this.lollipopFileInput1.Filter = "Image Files (*.jpg)|*.jpg";
+            this.lollipopFileInput1.FocusedColor = "#508ef5";
+            this.lollipopFileInput1.FontColor = "#314150";
+            this.lollipopFileInput1.IsEnabled = true;
+            this.lollipopFileInput1.Location = new System.Drawing.Point(270, 442);
+            this.lollipopFileInput1.MaxLength = 32767;
+            this.lollipopFileInput1.Name = "lollipopFileInput1";
+            this.lollipopFileInput1.ReadOnly = false;
+            this.lollipopFileInput1.Size = new System.Drawing.Size(259, 24);
+            this.lollipopFileInput1.TabIndex = 3;
+            this.lollipopFileInput1.Text = "File Input";
+            this.lollipopFileInput1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lollipopFileInput1.UseSystemPasswordChar = false;
+            // 
+            // lollipopFlatButton1
+            // 
+            this.lollipopFlatButton1.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopFlatButton1.FontColor = "#314150";
+            this.lollipopFlatButton1.Location = new System.Drawing.Point(360, 346);
+            this.lollipopFlatButton1.Name = "lollipopFlatButton1";
+            this.lollipopFlatButton1.Size = new System.Drawing.Size(143, 41);
+            this.lollipopFlatButton1.TabIndex = 4;
+            this.lollipopFlatButton1.Text = "lollipopFlatButton1";
+            // 
+            // lollipopFolderInPut1
+            // 
+            this.lollipopFolderInPut1.FocusedColor = "#508ef5";
+            this.lollipopFolderInPut1.FontColor = "#314150";
+            this.lollipopFolderInPut1.IsEnabled = true;
+            this.lollipopFolderInPut1.Location = new System.Drawing.Point(256, 393);
+            this.lollipopFolderInPut1.MaxLength = 32767;
+            this.lollipopFolderInPut1.Name = "lollipopFolderInPut1";
+            this.lollipopFolderInPut1.ReadOnly = false;
+            this.lollipopFolderInPut1.Size = new System.Drawing.Size(273, 24);
+            this.lollipopFolderInPut1.TabIndex = 5;
+            this.lollipopFolderInPut1.Text = "Folder Input";
+            this.lollipopFolderInPut1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lollipopFolderInPut1.UseSystemPasswordChar = false;
+            // 
+            // lollipopLabel1
+            // 
+            this.lollipopLabel1.AutoSize = true;
+            this.lollipopLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.lollipopLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lollipopLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.lollipopLabel1.Location = new System.Drawing.Point(186, 334);
+            this.lollipopLabel1.Name = "lollipopLabel1";
+            this.lollipopLabel1.Size = new System.Drawing.Size(95, 17);
+            this.lollipopLabel1.TabIndex = 6;
+            this.lollipopLabel1.Text = "lollipopLabel1";
+            // 
+            // lollipopProgressBar1
+            // 
+            this.lollipopProgressBar1.BGColor = "#314150";
+            this.lollipopProgressBar1.Location = new System.Drawing.Point(553, 476);
+            this.lollipopProgressBar1.Name = "lollipopProgressBar1";
+            this.lollipopProgressBar1.Size = new System.Drawing.Size(300, 10);
+            this.lollipopProgressBar1.TabIndex = 7;
+            this.lollipopProgressBar1.Text = "lollipopProgressBar1";
+            this.lollipopProgressBar1.Value = 76;
+            // 
+            // lollipopRadioButton1
+            // 
+            this.lollipopRadioButton1.AutoSize = true;
+            this.lollipopRadioButton1.CheckColor = "#508ef5";
+            this.lollipopRadioButton1.Location = new System.Drawing.Point(252, 270);
+            this.lollipopRadioButton1.Name = "lollipopRadioButton1";
+            this.lollipopRadioButton1.Size = new System.Drawing.Size(153, 19);
+            this.lollipopRadioButton1.TabIndex = 8;
+            this.lollipopRadioButton1.TabStop = true;
+            this.lollipopRadioButton1.Text = "lollipopRadioButton1";
+            this.lollipopRadioButton1.UseVisualStyleBackColor = true;
+            // 
+            // lollipopSmallCard1
+            // 
+            this.lollipopSmallCard1.FontColor = "#314150";
+            this.lollipopSmallCard1.Image = ((System.Drawing.Image)(resources.GetObject("lollipopSmallCard1.Image")));
+            this.lollipopSmallCard1.Info = "this is a dummy text";
+            this.lollipopSmallCard1.Location = new System.Drawing.Point(151, 168);
+            this.lollipopSmallCard1.Name = "lollipopSmallCard1";
+            this.lollipopSmallCard1.Size = new System.Drawing.Size(240, 52);
+            this.lollipopSmallCard1.TabIndex = 9;
+            this.lollipopSmallCard1.Text = "Demo By C# Ui Academy";
+            this.lollipopSmallCard1.ThumbnailColor = "#314150";
+            // 
+            // lollipopToggle1
+            // 
+            this.lollipopToggle1.AutoSize = true;
+            this.lollipopToggle1.Checked = true;
+            this.lollipopToggle1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.lollipopToggle1.EllipseBorderColor = "#3b73d1";
+            this.lollipopToggle1.EllipseColor = "#508ef5";
+            this.lollipopToggle1.Location = new System.Drawing.Point(344, 132);
+            this.lollipopToggle1.Name = "lollipopToggle1";
+            this.lollipopToggle1.Size = new System.Drawing.Size(47, 19);
+            this.lollipopToggle1.TabIndex = 10;
+            this.lollipopToggle1.Text = "lollipopToggle1";
+            this.lollipopToggle1.UseVisualStyleBackColor = true;
+            // 
+            // lollipopTextBox1
+            // 
+            this.lollipopTextBox1.FocusedColor = "#508ef5";
+            this.lollipopTextBox1.FontColor = "#999999";
+            this.lollipopTextBox1.IsEnabled = true;
+            this.lollipopTextBox1.Location = new System.Drawing.Point(39, 72);
+            this.lollipopTextBox1.MaxLength = 32767;
+            this.lollipopTextBox1.Multiline = false;
+            this.lollipopTextBox1.Name = "lollipopTextBox1";
+            this.lollipopTextBox1.ReadOnly = false;
+            this.lollipopTextBox1.Size = new System.Drawing.Size(300, 24);
+            this.lollipopTextBox1.TabIndex = 11;
+            this.lollipopTextBox1.Text = "Demo By C# Ui Academy";
+            this.lollipopTextBox1.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.lollipopTextBox1.UseSystemPasswordChar = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1027, 498);
+            this.Controls.Add(this.lollipopTextBox1);
+            this.Controls.Add(this.lollipopToggle1);
+            this.Controls.Add(this.lollipopSmallCard1);
+            this.Controls.Add(this.lollipopRadioButton1);
+            this.Controls.Add(this.lollipopProgressBar1);
+            this.Controls.Add(this.lollipopLabel1);
+            this.Controls.Add(this.lollipopFolderInPut1);
+            this.Controls.Add(this.lollipopFlatButton1);
+            this.Controls.Add(this.lollipopFileInput1);
+            this.Controls.Add(this.lollipopCheckBox1);
+            this.Controls.Add(this.lollipopCard1);
+            this.Controls.Add(this.lollipopButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LollipopButton lollipopButton1;
+        private LollipopCard lollipopCard1;
+        private LollipopCheckBox lollipopCheckBox1;
+        private LollipopFileInput lollipopFileInput1;
+        private LollipopFlatButton lollipopFlatButton1;
+        private LollipopFolderInPut lollipopFolderInPut1;
+        private LollipopLabel lollipopLabel1;
+        private LollipopProgressBar lollipopProgressBar1;
+        private LollipopRadioButton lollipopRadioButton1;
+        private LollipopSmallCard lollipopSmallCard1;
+        private LollipopToggle lollipopToggle1;
+        private LollipopTextBox lollipopTextBox1;
+    }
+}
+
